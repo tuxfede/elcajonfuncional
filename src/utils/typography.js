a7c57add84c0707ef379fefe651b80ec69f36998
flopez <flopez@glenseraerospace.com>
2018-11-12 12:56:37 +0100

    added vector logo. Updated typhografy util

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,4 +3,11 @@ import githubTheme from "typography-theme-github";
 
 const typography = new Typography(githubTheme);
 
-export default typography;
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
+export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
